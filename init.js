d6c6a135519ae0a5b06d007b66e063755ed1c9b8
Yuri Pinzetta <pinzetta@gmail.com>
2021-10-17 18:45:00 -0300

    teste

---
+++ init.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCFPSXTvkwBjJkIX2jfj7g15XNy60Y3kgM",
+    authDomain: "cursopwa-f509c.firebaseapp.com",
+    projectId: "cursopwa-f509c",
+    storageBucket: "cursopwa-f509c.appspot.com",
+    messagingSenderId: "502807671084",
+    appId: "1:502807671084:web:ec75db92923e9896303c89",
+    measurementId: "G-EG942DB0SD"
+};
+firebase.initializeApp(firebaseConfig);
