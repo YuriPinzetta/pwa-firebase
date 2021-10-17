0bd4728df256d5f989e7637a9776f481039409a8
Yuri Pinzetta <pinzetta@gmail.com>
2021-10-17 16:08:28 -0300

    ajuste no service-worker

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,39 @@
-var cacheName = 'son-pwa';
-var filesToCache = [
-    '/pwa-firebase/',
-    '/pwa-firebase/index.html',
-    '/pwa-firebase/css/app.css',
-    '/pwa-firebase/js/app.js'
+let cacheName = 'notes-son.v.1.0.0';
+let filesToCache = [
+    '/curso-firebase/',
+    '/curso-firebase/index.html',
+    '/curso-firebase/css/app.css',
+    '/curso-firebase/js/app.js'
 ];
 
-/* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
+self.addEventListener('install', function (e) {
+    console.log('[ServiceWorker] installer')
     e.waitUntil(
-        caches.open(cacheName).then(function(cache) {
+        caches.open(cacheName).then(function (cache) {
+            console.log('[ServiceWorker] Caching app shell')
             return cache.addAll(filesToCache);
+        })
+    );
+})
+
+self.addEventListener('activate', function (e) {
+    console.log('[ServiceWorker] activate')
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            return Promise.all(keyList.map(function (key) {
+                if(key !== cacheName){
+                    console.log('[ServiceWorker] Removing old cache')
+                    return cache.delete(key);
+                }
+            }))
         })
     );
 });
 
-/* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', function (e) {
+    console.log('[ServiceWorker] fetch', e.request.url)
     e.respondWith(
-        caches.match(e.request).then(function(response) {
+        caches.match(e.request).then(function (response) {
             return response || fetch(e.request);
         })
     );
